cfde83d7efde2f4205bab23938ff23426c83853f
yaniishsn <hassainiyanis2006@gmail.com>
2026-06-19 18:46:21 +0100

    small clearing

--- src/queen.java
+++ src/queen.java
@@ -1,6 +1,10 @@
 public class queen extends piece {
     static rook Arook = new rook(0, 0,true);
     static elephant AnElephant = new elephant(0, 0,true);
+    // even there are static , there would be no issue with minimax cz there would be no interfered calls 
+    // for each call , the queen sets the coords for it self and then directly modify the calculations then another takes the static ones
+    // there is no two calls in the same time (one after the other)
+
     // instead of creating a new ones each time , we just modify their coordinates 
      public queen(int row , int col,boolean isBlack) {
         super(row, col , isBlack) ;
